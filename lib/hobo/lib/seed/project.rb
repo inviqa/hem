9d1c88af1f88afcbdc20236e434506f26491b3a6
richardmiller <rmiller@inviqa.com>
2015-07-01 09:33:20 +0100

    Change use of .dev to .devw as it causes upsets when using chef 12

--- lib/hobo/lib/seed/project.rb
+++ lib/hobo/lib/seed/project.rb
@@ -15,7 +15,7 @@ module Hobo
           seed.update
           seed.export config[:project_path], config
           config[:seed][:version] = seed.version
-          config[:hostname] = "#{config[:name]}.dev"
+          config[:hostname] = "#{config[:name]}.devw"
           config[:asset_bucket] = "inviqa-assets-#{config[:name]}"
           config[:vm] = {
             :project_mount_path => "/vagrant"
