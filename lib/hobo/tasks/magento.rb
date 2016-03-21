2b709547e7d5ff7d906fd6f7f26a171fc601ad17
Andy Thompson <athompson@inviqa.com>
2016-03-21 15:46:07 +0000

    Replace magento tasks with deprecation message and plugin

--- lib/hobo/tasks/magento.rb
+++ lib/hobo/tasks/magento.rb
@@ -1,3 +1,4 @@
 Hem.ui.warning 'require \'hobo/tasks/magento\' in Hobofile is deprecated, and will be removed in a future release.'
+HOBO_TASKS_MAGENTO_DEPRECATION=1
 
 require_relative '../../hem/tasks/magento'
