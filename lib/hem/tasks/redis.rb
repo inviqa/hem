e3d65d7919096160692fb59ec22e330fa397aac6
Andy Thompson <athompson@inviqa.com>
2016-04-07 15:16:24 +0100

    Add a task to flush redis dbs

--- lib/hem/tasks/redis.rb
+++ lib/hem/tasks/redis.rb
@@ -10,4 +10,17 @@ namespace :redis do
     Hem.ui.success "Connecting..." if STDOUT.tty?
     exec command
   end
+
+  desc "Flush some or all redis dbs"
+  argument :dbs, optional: true, default: {}, as: Array
+  task :flush do |task, args|
+    if args[:dbs].empty?
+      run 'redis-cli FLUSHALL'
+    else
+      args[:dbs].each do |db|
+        Hem.ui.success "Flushing redis db #{db}" if STDOUT.tty?
+        shell create_command('redis-cli', auto_echo: true).pipe("SELECT #{db}\nFLUSHDB\n")
+      end
+    end
+  end
 end
