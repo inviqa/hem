d0cafe9c656df0c9c6f7381e157bc7deeeef7c63
Andy Thompson <athompson@inviqa.com>
2016-05-25 14:43:26 +0100

    Update hem exec to use new arguments format

--- lib/hem/tasks/exec.rb
+++ lib/hem/tasks/exec.rb
@@ -1,3 +1,5 @@
-task 'exec' do
-  exec(*$HEM_ARGV)
+argument :command
+argument 'args', optional: true, default: {}, as: Array
+task 'exec' do |task, args|
+  exec [args[:command], args[:command]], *args[:args]
 end
