b9e9c6e12d105c34dc7e78b24f5b08df4f4a3d6d
Andy Thompson <athompson@inviqa.com>
2016-05-31 14:46:28 +0100

    Remove Hem bundler self reference
    The use of Bundler is having trouble requiring multiple separate definitions

--- lib/hem/setup.rb
+++ lib/hem/setup.rb
@@ -2,6 +2,5 @@ require 'bundler'
 require_relative 'version'
 require_relative 'plugins'
 
-Hem::Plugins.new(__dir__, '.noGemfile', false).define do
-  gem 'hem', Hem::VERSION, :path => File.expand_path(File.join('..', '..', '..'), __FILE__)
-end.require
+$:.push File.expand_path(File.join("..", ".."), __FILE__)
+require 'hem'
