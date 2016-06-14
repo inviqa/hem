367a3f0db5ce9f3a356d912a3969a244a0158dc2
Andy Thompson <athompson@inviqa.com>
2016-06-14 13:24:16 +0100

    Update hem/setup to detach from bundle exec

--- lib/hem/setup.rb
+++ lib/hem/setup.rb
@@ -1,6 +1,12 @@
-require 'bundler'
+if defined?(Bundler)
+  Bundler.with_clean_env do
+    exec [$PROGRAM_NAME,$PROGRAM_NAME], *ARGV
+  end
+end
+
 require_relative 'version'
 require_relative 'plugins'
 
 $:.push File.expand_path(File.join("..", ".."), __FILE__)
+require 'bundler'
 require 'hem'
