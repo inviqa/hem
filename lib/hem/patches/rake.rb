5c9a0c49ff466f4900f0e738f5f86c70cd36872a
Kieren Evans <kevans@inviqa.com>
2016-10-09 22:14:59 +0100

    Fix before DSL to place new tasks before the prerequsites of the old task.

--- lib/hem/patches/rake.rb
+++ lib/hem/patches/rake.rb
@@ -13,7 +13,7 @@ module Rake
       old_task = Rake.application.instance_variable_get('@tasks').delete(task_name)
 
       Hem::Metadata.to_store task_name
-      task task_name => old_task.prerequisites | new_tasks do
+      task task_name => new_tasks | old_task.prerequisites  do
         new_task.call unless new_task.nil?
         old_task.invoke
       end
